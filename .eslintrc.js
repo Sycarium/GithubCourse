163c68332059326c07bb955e557586ecc5bc3dba
Sycarium <95645771+Sycarium@users.noreply.github.com>
2024-04-04 23:10:15 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:react/recommended"
     ],
     "overrides": [
         {
@@ -20,13 +20,12 @@ module.exports = {
             }
         }
     ],
-    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
     "plugins": [
-        "@typescript-eslint"
+        "react"
     ],
     "rules": {
     }
